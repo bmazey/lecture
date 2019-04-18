c1ccbe4cd2e6fd34f52c8723d249f541fd1f474b
Brandon <b.mazey@nyu.edu>
2019-04-18 14:49:37 -0400

    adding CORS headers to controller

--- src/main/java/org/nyu/rumors/controller/RumorController.java
+++ src/main/java/org/nyu/rumors/controller/RumorController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 @RequestMapping("api")
 public class RumorController {
